4f6b49246dcba36aff870771ee21c8aa73efd600
ted3000 <51426025+ted3000@users.noreply.github.com>
2019-06-05 19:07:08 -0500

    Add files via upload

---
+++ ted/conexion.php
@@ -0,0 +1,9 @@
+
+<?php
+
+$servidor="LOcalhost";
+$Usuario="root";
+$Clave="";
+$Base="Hotel thanos";
+$Conectar= new mysqli($servidor, $Usuario; $Clave; $Base);
+$Conectar -> set_character('utf8');
